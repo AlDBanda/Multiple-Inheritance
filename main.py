5b34d3e3be360eed4e00f84bdfa3a83b690d3a06
ALDBanda <88377643+AlDBanda@users.noreply.github.com>
2021-08-21 20:09:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from app import Resident
+
+resident = Resident(name='Jennifer', age='28', id=23)
+
+#resident.print_name()
+#resident.print_age()
+#resident.print_id()
+
+resident.title()
+print(Resident.__mro__)
+
+#method resolution order
+
+
